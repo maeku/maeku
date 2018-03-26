b6116fd328472805700cb811c89253a09fe39841
Benjamin Willems <bw@benjaminwil.info>
2018-03-25 22:40:40 -0700

    Add author_id field; integrate application user

--- app/controllers/journal/application_controller.rb
+++ app/controllers/journal/application_controller.rb
@@ -1,5 +1,20 @@
 module Journal
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
+
+    helper_method :current_author, :logged_in?
+
+    private
+
+    # wip: uses session variables to check who the current author is. this
+    # should not depend on a devise-style "current_user" because the
+    # application may not use devise.
+    def current_author
+    end
+
+    # wip: if there is a current_author, then logged_in is true.
+    def logged_in?
+    end
+
   end
 end
