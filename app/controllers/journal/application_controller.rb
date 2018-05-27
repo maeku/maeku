c553aa95232a25e571031bfb94ba853ae263b82c
Benjamin Willems <bw@benjaminwil.info>
2018-05-27 15:15:38 -0700

    Create dummy User model

--- app/controllers/journal/application_controller.rb
+++ app/controllers/journal/application_controller.rb
@@ -2,17 +2,15 @@ module Journal
   class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
 
-    helper_method :current_author
-
     private
 
     def current_author
-      if defined? Journal.current_author
-        Journal.current_author.constantize
-      elsif defined? current_user
+      if respond_to? :current_user
         current_user
+      elsif Journal.current_author
+        Journal.current_author
       else
-        raise "Your application needs a 'current_user' method. This method is "/
+        raise "Your application needs a 'current_user' method. This method is "\
           "typically provided if you use an authentication gem like devise."
       end
     end
