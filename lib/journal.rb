91a66fede666c88c5ecd20f08e7cdc6de2acb3b9
Benjamin Willems <bw@benjaminwil.info>
2018-06-02 22:10:37 -0700

    Constantize mattr_accessors

--- lib/journal.rb
+++ lib/journal.rb
@@ -2,13 +2,13 @@ require "journal/engine"
 
 module Journal
   mattr_accessor :author_class
+  mattr_accessor :current_author
 
   def self.author_class
-    if @@author_class.is_a?(Class)
-      raise "Journal.author_class must be a String or Symbol object, not a "/
-        "Class object."
-    elsif @@author_class.is_a?(String) || @@author_class.is_a?(Symbol)
-      @@author_class.to_s.constantize
-    end
+    @@author_class.to_s.constantize
+  end
+
+  def self.current_author
+    @@current_author.to_s.constantize
   end
 end
