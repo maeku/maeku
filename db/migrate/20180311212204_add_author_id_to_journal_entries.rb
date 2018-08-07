16ace67b80589647d2cf9de41947de3280638f67
Benjamin Willems <bw@benjaminwil.info>
2018-08-06 20:37:35 -0700

    Ensure entry author_ids are never nil
    An entry should not be able to be saved if there is no author ID. In the
    author_id migration file, I `add_column` and then `change_column` right
    after. This is because SQLite produces an error if you add a column with
    `null: false` and no `default: <value>`. This simply gets around that
    error. *shrug*

--- db/migrate/20180311212204_add_author_id_to_journal_entries.rb
+++ db/migrate/20180311212204_add_author_id_to_journal_entries.rb
@@ -1,5 +1,6 @@
 class AddAuthorIdToJournalEntries < ActiveRecord::Migration[5.1]
   def change
     add_column :journal_entries, :author_id, :integer
+    change_column :journal_entries, :author_id, :integer, null: false
   end
 end
