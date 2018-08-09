dfe2513a7c80b314e8aec86fafab40bc2b33f377
Benjamin Willems <bw@benjaminwil.info>
2018-08-08 18:54:16 -0700

    Validate presence of entry content and author_id
    This commit changes the Entry model validations and adds tests for the
    Entry model.

--- test/models/journal/entry_test.rb
+++ test/models/journal/entry_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 module Journal
   class EntryTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    
+    setup do
+      entry = "I saw spoiled fruit on the lawn."
+      author_id = 1
+      @entry = Entry.new(entry: entry, author_id: author_id) 
+    end
+    
+    test "should create new entry if entry and author_id exist" do
+      assert @entry.save
+    end
+    
+    test "should not create new entry if entry is nil" do
+      @entry.entry = nil
+      assert_not @entry.save
+    end
+    
+    test "should not create new entry if author_id is nil" do
+      @entry.author_id = nil
+      assert_not @entry.save
+    end
+    
   end
 end
