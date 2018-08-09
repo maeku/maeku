e5349795a30df7e461a15d14e78fee7ba2d083de
Benjamin Willems <bw@benjaminwil.info>
2018-08-08 18:54:16 -0700

    Validate presence of entry content and author_id
    This commit changes the Entry model validations and adds tests for the
    Entry model.

--- app/models/journal/entry.rb
+++ app/models/journal/entry.rb
@@ -1,13 +1,7 @@
 module Journal
   class Entry < ApplicationRecord
     belongs_to :author, class_name: Journal.author_class.to_s
-
-    before_validation :set_author
-
-    private
-
-    def set_author
-      self.author = Journal.author_class.find_or_create_by(id: author_id)
-    end
+    validates :entry, presence: true
+    validates :author_id, presence: true
   end
 end
