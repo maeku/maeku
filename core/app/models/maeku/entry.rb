8b5638036966a56eb541c3285ab1654281398982
Benjamin Willems <bw@benjaminwil.info>
2018-09-08 13:49:00 -0700

    Create Maeku::EntryContent model
    This moves some of the `Entry` model logic to the `EntryContent` model.
    Having a separate `EntryContent` table in the database is attractive for
    a number of reasons:
    
    1. In the future, it is possible that not all entries will have text
       content.
    2. It makes it simpler if we wanted to export _just_ the text content of
       an author's journal.
    3. It hides sensitive journal entry content from non-malicious admins
       who need to debug some `Entries` for some reason.

--- core/app/models/maeku/entry.rb
+++ core/app/models/maeku/entry.rb
@@ -1,7 +1,11 @@
 module Maeku
   class Entry < ApplicationRecord
     belongs_to :author, class_name: Maeku.author_class.to_s
-    validates :entry, presence: true
+    has_one :entry_content
+
+    accepts_nested_attributes_for :entry_content, update_only: true
+
     validates :author_id, presence: true
+    validates :entry_content, presence: true
   end
 end
