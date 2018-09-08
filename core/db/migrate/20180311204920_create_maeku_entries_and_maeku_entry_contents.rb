5d00c1ebd9c50fb2bbdb7d61597ae638c09eaebf
Benjamin Willems <bw@benjaminwil.info>
2018-09-08 13:49:00 -0700

    Create Maeku::EntryContent model
    This moves some of the `Entry` model logic to the `EntryContent` model.
    Having a separate `EntryContent` table in the database is attractive for
    a number of reasons:
    
    1. In the future, it is possible that not all entries will have text
       content.
    2. It makes it simpler if we wanted to export _just_ the text content of
       an author's journal.
    3. It hides sensitive journal entry content from non-malicious admins
       who need to debug some `Entries` for some reason.

---
+++ core/db/migrate/20180311204920_create_maeku_entries_and_maeku_entry_contents.rb
@@ -0,0 +1,20 @@
+class CreateMaekuEntriesAndMaekuEntryContents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :maeku_entries
+
+    add_column :maeku_entries, :author_id, :integer
+    change_column :maeku_entries, :author_id, :integer, null: false
+
+    add_column :maeku_entries, :created_at, :datetime
+    change_column :maeku_entries, :created_at, :datetime, null: false
+
+    add_column :maeku_entries, :updated_at, :datetime
+    change_column :maeku_entries, :updated_at, :datetime, null: false
+
+    create_table :maeku_entry_contents
+
+    add_column :maeku_entry_contents, :content, :text
+
+    add_column :maeku_entry_contents, :entry_id, :integer
+  end
+end
