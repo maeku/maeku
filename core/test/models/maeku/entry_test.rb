c0044a74eb12636906cea271a229d96f7c93a9a5
Benjamin Willems <bw@benjaminwil.info>
2018-09-23 14:47:06 -0700

    Revise entry tests to account for EntryContents

--- core/test/models/maeku/entry_test.rb
+++ core/test/models/maeku/entry_test.rb
@@ -2,26 +2,27 @@ require 'test_helper'
 
 module Maeku
   class EntryTest < ActiveSupport::TestCase
-    
+
     setup do
-      entry = "I saw spoiled fruit on the lawn."
       author_id = 1
-      @entry = Entry.new(entry: entry, author_id: author_id) 
+      entry_content = "I saw spoiled fruit on the lawn."
+      @entry = Entry.new(author_id: author_id)
+      @entry.assign_attributes(entry_content_attributes: { content: entry_content })
     end
-    
+
     test "should create new entry if entry and author_id exist" do
       assert @entry.save
     end
-    
+
     test "should not create new entry if entry is nil" do
-      @entry.entry = nil
+      @entry.entry_content = nil
       assert_not @entry.save
     end
-    
+
     test "should not create new entry if author_id is nil" do
       @entry.author_id = nil
       assert_not @entry.save
     end
-    
+
   end
 end
