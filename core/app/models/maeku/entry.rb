5e0d80e13ec5f4e614bf05f3ddb00b48980d43a6
Benjamin Willems <bw@benjaminwil.info>
2018-11-28 20:37:55 -0800

    Add content type validations for entry types
    At the model level:
    
    - An `Entry` should accept file attachments that are audio files,
      images, or videos in ActiveStorage.
    - An `EntryType::*` requires the type of file attachment it's named
      after. For example: an `Audio` requires an `:audio_file` attachment.
    
    I used the [MDN's incomplete list of MIME types][1] to come up with the
    valid `content_type`s.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types

--- core/app/models/maeku/entry.rb
+++ core/app/models/maeku/entry.rb
@@ -1,11 +1,22 @@
+require 'active_storage_validations'
+
 module Maeku
   class Entry < ApplicationRecord
     belongs_to :author, class_name: Maeku.author_class.to_s
+
     has_one :entry_content
+
+    has_many_attached :audio_files
+    has_many_attached :image_files
+    has_many_attached :video_files
 
     accepts_nested_attributes_for :entry_content, update_only: true
 
     validates :author_id, presence: true
     validates :entry_content, presence: true
+
+    validates :audio_files, content_type: ['audio/aac', 'audio/midi', 'audio/mpeg', 'audio/webm', 'audio/x-wav']
+    validates :image_files, content_type: ['image/gif', 'image/jpeg', 'image/jpg', 'image/png']
+    validates :video_files, content_type: ['video/mp4', 'video/webm']
   end
 end
