ab0b634039764f4566bb75077a840243b535599a
Benjamin Willems <bw@benjaminwil.info>
2018-11-28 19:58:18 -0800

    Get rid of SQLite 3 deprecation warnings
    I was asked to represent booleans as integers if I'm testing against a
    SQLite 3 database. Nice.

--- core/test/dummy/config/application.rb
+++ core/test/dummy/config/application.rb
@@ -7,12 +7,8 @@ require "maeku"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.active_record.sqlite3.represent_boolean_as_integer = true
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
 
