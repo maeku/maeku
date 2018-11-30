4acdf824453d1c0ed191810ce1ded1731073bf5c
Benjamin Willems <bw@benjaminwil.info>
2018-11-29 20:24:25 -0800

    Test entry file attachment validation
    This commit adds basic tests for entry file attachments. Basically: an
    `Entry` should accept any file attachment, and a `EntryType::*` should
    *only* save entries with a specific type of file attachment attached.
    i.e. `EntryType::Audio` requires an audio file to be attached before
    save.

--- core/test/models/maeku/entry_test.rb
+++ core/test/models/maeku/entry_test.rb
@@ -24,5 +24,17 @@ module Maeku
       assert_not @entry.save
     end
 
+    test "should accept audio, image, and video attachments" do
+      public_dir = Rails.root.join('public')
+      audio = File.open("#{public_dir}/credits-music.mp3")
+      image = File.open("#{public_dir}/grandparents.jpeg")
+      video = File.open("#{public_dir}/dramatic-exit.mp4")
+
+      @entry.audio_files.attach(io: audio, filename: 'credits-music.mp3', content_type: 'audio/mpeg')
+      @entry.image_files.attach(io: image, filename: 'grandparents.jpeg', content_type: 'image/jpeg')
+      @entry.video_files.attach(io: video, filename: 'dramatic-exit.mp4', content_type: 'video/mp4')
+      assert @entry.save!
+    end
+
   end
 end
