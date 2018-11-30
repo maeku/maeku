8807956191408164622f485c7a5ffe4867d283c0
Benjamin Willems <bw@benjaminwil.info>
2018-11-29 21:17:19 -0800

    Configure dummy app's Active Storage storage

--- core/test/dummy/config/application.rb
+++ core/test/dummy/config/application.rb
@@ -8,6 +8,7 @@ require "maeku"
 module Dummy
   class Application < Rails::Application
     config.active_record.sqlite3.represent_boolean_as_integer = true
+    config.active_storage.service = :local
     config.load_defaults 5.1
   end
 end
