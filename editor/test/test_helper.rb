e7be01fde727410287e489e1d60f6b17a17215ed
Benjamin Willems <bw@benjaminwil.info>
2018-12-15 14:20:16 -0800

    Flesh out dummy app configuration
    1. The dummy app should be used for the `maeku_editor` test suite, as
       well.
    2. The dummy app `config/environments/development.rb` env configuation
       got streamlined and tweaked.
    3. The dummy app route got changed to `/`. This is to avoid a problem
       where GraphiQL does not obey the `/custom-app` root mount. For more
       information see #11. Hopefully this is easy to fix.

--- editor/test/test_helper.rb
+++ editor/test/test_helper.rb
@@ -1,2 +1,22 @@
-maeku_core_dir = Gem::Specification.find_by_name("maeku_core").gem_dir
-require "#{maeku_core_dir}/test/test_helper.rb"
+ENV["RAILS_ENV"] = "test"
+
+CORE_DIR = "../../../core"
+CORE_TEST_DIR = "#{CORE_DIR}/test"
+
+require File.expand_path("#{CORE_TEST_DIR}/dummy/config/environment.rb", __FILE__)
+
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("#{CORE_TEST_DIR}/dummy/db/migrate", __FILE__)]
+ActiveRecord::Migrator.migrations_paths << File.expand_path('#{CORE_DIR}/core/db/migrate', __FILE__)
+ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
+
+require "rails/test_help"
+
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixtures :all
+end
