d0ab3bf025ea15b6124b7a96dd7fc9bb431bf3c6
Benjamin Willems <bw@benjaminwil.info>
2018-12-17 00:56:12 -0800

    Dismantle query and mutation type scaffolding
    Let's call the query and mutation types `MutationSchema` and
    `QuerySchema`, and move them up a directory so that it's clear that they
    are parents.

--- core/app/graphql/maeku/maeku_schema.rb
+++ core/app/graphql/maeku/maeku_schema.rb
@@ -1,8 +1,10 @@
 require 'graphql'
+require_relative 'query_schema'
+require_relative 'mutation_schema'
 
 module Maeku
   class MaekuSchema < ::GraphQL::Schema
-    # mutation(Types::MutationType)
-    query(Types::QueryType)
+    query QuerySchema
+    mutation MutationSchema
   end
 end
