201627a71fb83389a2523dd7f8a4f6c04776f9a9
Benjamin Willems <bw@benjaminwil.info>
2018-12-17 01:01:33 -0800

    Expand initial entry object API

--- core/app/graphql/maeku/types/entry_type.rb
+++ core/app/graphql/maeku/types/entry_type.rb
@@ -1,8 +1,10 @@
-Maeku::Types::EntryType = GraphQL::ObjectType.define do
-
-  name 'Entry'
-
-  field :id, !types.ID
-  field :type, types.String
-
+module Maeku
+  class Types::EntryType < Types::BaseType
+    field :id, ID, null: false
+    field :type, String, null: true
+    field :entry_content, Types::EntryContentType, null: true
+    # todo ~ what's the best way to include entry attachments in api queries?
+    # todo ~ i think the answer is urls.
+    # field :attachments, [String], null: true
+  end
 end
