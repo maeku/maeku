ef27b5917ab3e128eea1dbf7ddcf20d7f469f7e4
Benjamin Willems <bw@benjaminwil.info>
2018-12-17 21:11:44 -0800

    Define allEntries and myEntries fields

--- core/app/graphql/maeku/query_schema.rb
+++ core/app/graphql/maeku/query_schema.rb
@@ -3,13 +3,22 @@ module Maeku
     graphql_name "Query"
     description "The query root for this API."
 
-    field :entries, [Types::EntryType], null: true do
+    field :all_entries, [Types::EntryType], null: true do
       description "Returns information about entries."
       argument :limit, Int, default_value: 30, required: false
     end
-    
-    def entries(args) 
+
+    field :my_entries, [Types::EntryType], null: true do
+      description "Returns entries posted by the current author."
+      argument :limit, Int, default_value: 30, required: false
+    end
+
+    def all_entries(args)
       Entry.limit(args[:limit]).order(updated_at: :desc)
+    end
+
+    def my_entries(args)
+      Entry.entries_for(context[:current_author]).limit(args[:limit]).order(updated_at: :desc)
     end
   end
 end
