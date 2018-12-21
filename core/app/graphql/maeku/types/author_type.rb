ae3219b63cc0725984653f36d2d7b994c5cbb6ae
Benjamin Willems <bw@benjaminwil.info>
2018-12-21 13:47:53 -0700

    Add AuthorType (with dynamic field names)
    Because Maeku does not provide user authentication, it depends on the
    application administrator to add their own. Probably Devise, but maybe
    some other thing like Passwordless or Clearance.
    
    Because we don't know what fields will be on the `User`, `Author`, or
    whatever the user model is called, we need to account for this and not
    make many assumptions in our `AuthorType` design.
    
    We'll do this by looking at the `Maeku.author_class`'s columns and
    picking out any non-sensitive ones that are not date/time-oriented.
    
    Right now, I only explicitly look for columns with the term "password"
    in them, because all of the sensitive fields provided by Devise include
    that word.

---
+++ core/app/graphql/maeku/types/author_type.rb
@@ -0,0 +1,14 @@
+module Maeku
+  class Types::AuthorType < Types::BaseType
+    description "Type for the application-provided #{Maeku.author_class.name} class."
+
+    Maeku.author_class.columns.each do |column|
+      ignored_column_types = [:date, :datetime, :time]
+      unless ignored_column_types.include? column.type
+        type = const_get(column.type.to_s.capitalize)
+        field column.name.to_sym, type, null: true if column.name !~ /password/
+      end
+    end
+
+  end
+end
