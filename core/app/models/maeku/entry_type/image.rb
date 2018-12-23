e881776760bdc54423a1ae0ae7d8ce9fbd5c82dc
Benjamin Willems <bw@benjaminwil.info>
2018-12-23 11:53:40 -0700

    Gallery classes renamed to "Image"
    This will make my life more DRY in the long run. Before this, galleries
    had `image_files` attached. But renaming the class from `Gallery` to
    `Image` allows the `Image` to relate to `image_files`.

--- core/app/models/maeku/entry_type/image.rb
+++ core/app/models/maeku/entry_type/image.rb
@@ -1,5 +1,5 @@
 module Maeku
-  class EntryType::Gallery < Entry
+  class EntryType::Image < Entry
     validates :image_files, attached: true, content_type: ['image/gif', 'image/jpeg', 'image/jpg', 'image/png']
   end
 end
