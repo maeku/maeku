add21d7c7b03792cb007e7dd42a306b1442868dc
Benjamin Willems <bw@benjaminwil.info>
2018-12-23 18:04:03 -0700

    Harden initial query schema
    There is now a clearer pattern for our root queries:
    
    -all[records]
    -currentAuthor[records]
    
    So for now, we have four root queries:
    
    - allAuthors
    - allEntries
    - currentAuthor
    - currentAuthorEntries

--- core/app/graphql/maeku/query_schema.rb
+++ core/app/graphql/maeku/query_schema.rb
@@ -3,20 +3,30 @@ module Maeku
     graphql_name "Query"
     description "The query root for this API."
 
+    field :all_authors, [Types::AuthorType], null: true do
+      description "Returns information about all authors. Note that the " \
+        "class is not provided by Maeku, so author fields differ from "   \
+        "application to application."
+    end
+
     field :all_entries, [Types::EntryType], null: true do
       description "Returns information about all entry records."
       argument :limit, Int, default_value: 30, required: false
     end
 
     field :current_author, Types::AuthorType, null: true do
-      description "Returns data about the current user. Note that the " \
-      "class is not provided by Maeku, so author fields differ from "   \
+      description "Returns data about the current user. Note that the "   \
+      "class is not provided by Maeku, so author fields differ from "     \
       "application to application."
     end
 
-    field :my_entries, [Types::EntryType], null: true do
+    field :current_author_entries, [Types::EntryType], null: true do
       description "Returns entries posted by the current author."
       argument :limit, Int, default_value: 30, required: false
+    end
+
+    def all_authors
+      Maeku.author_class.all
     end
 
     def all_entries(args)
@@ -27,7 +37,7 @@ module Maeku
       context[:current_author]
     end
 
-    def my_entries(args)
+    def current_author_entries(args)
       Entry.entries_for(context[:current_author]).limit(args[:limit]).order(updated_at: :desc)
     end
   end
