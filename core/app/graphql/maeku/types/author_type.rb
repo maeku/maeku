22c9206cbaa49cbfa8ec8967c8c8d0450d527acb
Benjamin Willems <bw@benjaminwil.info>
2018-12-23 17:58:00 -0700

    Add entries field to GraphQL's AuthorType
    Now, you can get a list of allAuthors with a list of all entries they've
    created.

--- core/app/graphql/maeku/types/author_type.rb
+++ core/app/graphql/maeku/types/author_type.rb
@@ -2,12 +2,18 @@ module Maeku
   class Types::AuthorType < Types::BaseType
     description "Type for the application-provided #{Maeku.author_class.name} class."
 
+    field :entries, [Types::EntryType], null: true
+
     Maeku.author_class.columns.each do |column|
       ignored_column_types = [:date, :datetime, :time]
       unless ignored_column_types.include? column.type
         type = const_get(column.type.to_s.capitalize)
         field column.name.to_sym, type, null: true if column.name !~ /password/
       end
+    end
+
+    def entries
+      Entry.where(author_id: id)
     end
 
   end
