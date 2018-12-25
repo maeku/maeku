0e8a0593036e1812e4152780d4ce8f34e2b4c86c
Benjamin Willems <bw@benjaminwil.info>
2018-12-24 18:28:25 -0700

    Add react-rails and its configuration

--- editor/lib/maeku_editor/engine.rb
+++ editor/lib/maeku_editor/engine.rb
@@ -1,7 +1,16 @@
+require 'react-rails'
+
 module Maeku
   module Editor
     class Engine < ::Rails::Engine
       isolate_namespace ::Maeku::Editor
+
+      initializer("maeku-editor.react-rails") do |app|
+        app.config.react.variant = Rails.env.to_s
+        app.config.react.server_renderer_options = {
+          files: ["maeku/editor/server_rendering.js"]
+        }
+      end
     end
   end
 end
