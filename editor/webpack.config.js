84e2a1ce1b08debaa6f2ab7caade962b2c31f26f
Benjamin Willems <bw@benjaminwil.info>
2019-01-12 15:58:36 -0800

    Add initial Babel and Webpack configuration

---
+++ editor/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './javascript/EditorApplication.jsx',
+  devServer: {
+    hotOnly: true,
+    port: 3200,
+    publicPath: 'http://localhost:3000/editor/',
+    writeToDisk: true,
+  },
+  mode: 'development',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'vendor/assets/javascripts/maeku/editor/'),
+    filename: 'bundle.js'
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+};
