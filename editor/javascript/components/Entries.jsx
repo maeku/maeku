f71e849f3fb3c69ca62ef87ad37a9fb108ddfc18
Benjamin Willems <bw@benjaminwil.info>
2019-01-20 16:20:18 -0800

    Add initial entry components
    There are two basic entry components:
    
    `Entries`: This should display a list of entries. Right now it just
    requires an `id` and `content` for each entry: but this will definitely
    get more complex.
    `Entry`: A single entry being displayed. I will need to actually split
    this out into a couple different components down the line: one for index
    views and one for single entry views. Maybe more.

---
+++ editor/javascript/components/Entries.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import Entry from './Entry';
+
+class Entries extends React.Component {
+  render() {
+    const entries = this.props.entries.map(entry => (
+      <Entry key={entry.id} content={entry.content} />
+    ));
+
+    return <div className="entries">{entries}</div>;
+  }
+}
+
+export default Entries;
