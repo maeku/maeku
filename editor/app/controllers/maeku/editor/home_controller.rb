c5c1f5cb11f16127bd1acc0e2b8fe98764205f3c
Benjamin Willems <bw@benjaminwil.info>
2019-07-03 20:30:59 -0700

    Add list of entries to /editor/index
    This commit illustrates how React components should live within ERB
    templates.

--- editor/app/controllers/maeku/editor/home_controller.rb
+++ editor/app/controllers/maeku/editor/home_controller.rb
@@ -1,7 +1,14 @@
 module Maeku
   class Editor::HomeController < Editor::BaseController
 
-    def index; end
+    def index
+      @entries = Entry.all.where(author_id: current_author).map do |entry|
+        {
+          :id => entry.id,
+          :content => entry.entry_content.content
+        }
+      end
+    end
 
   end
 end
