9fbf2a1c17faa3ee48cb5500c2604a379b1098e8
Benjamin Willems <bw@benjaminwil.info>
2019-07-14 14:11:20 -0700

    Start compiling stylesheets via Webpack
    This commit configures Webpack to compile and serve SCSS assets
    separately from the React components. Unfortunately, there's a lot of
    scaffolding required to make set this up: new NPM packages, more dummy
    app initialization (which will have to be set up by a Rails generator
    someday), and more weird Webpack configuration!

--- dummy/config/initializers/assets.rb
+++ dummy/config/initializers/assets.rb
@@ -2,4 +2,8 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w( maeku/editor/editor.js )
+Rails.application.config.assets.precompile += %w(
+  maeku/editor/editor.css
+  maeku/editor/editor.js
+)
+
