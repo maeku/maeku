d76a487622255d5165b9d0e97bec91e6a83a03ce
Benjamin Willems <bw@benjaminwil.info>
2019-07-22 23:20:19 -0700

    Before initialization, load engine locales

--- core/lib/maeku/engine.rb
+++ core/lib/maeku/engine.rb
@@ -1,5 +1,9 @@
 module Maeku
   class Engine < ::Rails::Engine
-    isolate_namespace Maeku 
+    isolate_namespace Maeku
+
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
   end
 end
