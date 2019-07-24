1e121c6772e6ff225111b719ed44710ad4493e1e
Benjamin Willems <bw@benjaminwil.info>
2019-07-23 18:34:12 -0700

    Move GraphQL field definition to near its method
    The AuthorType :entries field should be placed after the
    dynamically-generated AuthorType fields. This makes reading the file a
    little bit more straightforward.

--- core/app/graphql/maeku/types/author_type.rb
+++ core/app/graphql/maeku/types/author_type.rb
@@ -2,8 +2,6 @@ module Maeku
   class Types::AuthorType < Types::BaseType
     description "Type for the application-provided #{Maeku.author_class.name} class."
 
-    field :entries, [Types::EntryType], null: true
-
     Maeku.author_class.columns.each do |column|
       ignored_column_types = [:date, :datetime, :time]
       unless ignored_column_types.include? column.type
@@ -12,9 +10,10 @@ module Maeku
       end
     end
 
+    field :entries, [Types::EntryType], null: true
+
     def entries
       Entry.where(author_id: id)
     end
-
   end
 end
