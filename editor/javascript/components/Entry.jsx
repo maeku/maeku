f5cb15a16d717b7af262d282f049697a49d06022
Benjamin Willems <bw@benjaminwil.info>
2019-08-19 20:11:35 -0700

    Add EntryTitle component
    Not much to say about this. I can see the EntryTitle becoming more
    complicated UI-wise, so best to extract it into its own component
    already.

--- editor/javascript/components/Entry.jsx
+++ editor/javascript/components/Entry.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 
+import EntryTitle from './EntryTitle';
+
 class Entry extends React.Component {
   render() {
     return (
       <article key={this.props.id} className="entry">
+        <EntryTitle title={this.props.title}/>
         <p>{this.props.content}</p>
       </article>
     );
