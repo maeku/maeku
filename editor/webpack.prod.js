bb62430500bdd22f919a3f96e778279c3110e4a0
Benjamin Willems <bw@benjaminwil.info>
2019-08-20 20:43:15 -0700

    Create Webpack configuration for production mode

---
+++ editor/webpack.prod.js
@@ -0,0 +1,40 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './javascript/main.jsx',
+  devServer: {
+    hotOnly: true,
+    port: 3200,
+    publicPath: 'http://localhost:3000/editor/',
+    writeToDisk: true,
+  },
+  mode: 'production',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'editor.css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {presets: ['@babel/env']},
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'vendor/assets/javascripts/maeku/editor/'),
+    filename: 'editor.js',
+  },
+  resolve: {extensions: ['*', '.js', '.jsx']},
+};
