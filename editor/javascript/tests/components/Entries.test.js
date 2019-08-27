322757167a6a8638821b296151cc0f2dc3f7d77f
Benjamin Willems <bw@benjaminwil.info>
2019-08-27 09:27:26 -0700

    Test Entry and Entries components with Enzyme

--- editor/javascript/tests/components/Entries.test.js
+++ editor/javascript/tests/components/Entries.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Entries from '../../components/Entries.jsx';
+import {configure, shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({adapter: new Adapter()});
+
+import Entries from '../../components/Entries.jsx';
 
 const entries = [
   {
@@ -16,7 +20,39 @@ const entries = [
   },
 ];
 
-test("the Entries component displays many entries", () => {
+const entriesWrapper = mount(<Entries entries={entries} />);
+
+test('the Entries component displays many entries', () => {
   const component = renderer.create(<Entries entries={entries} />).toJSON();
   expect(component).toMatchSnapshot();
+});
+
+test('when no Entry is clicked, no EntryControls are displayed', () => {
+  expect(!entriesWrapper.find('.controls'));
+});
+
+test("when an Entry is clicked, only the active Entry's EntryControls are \
+  displayed", () => {
+  const firstEntry = entriesWrapper.find('.entry').at(0);
+  firstEntry.simulate('click');
+  expect(entriesWrapper.find('.controls')).toHaveLength(1);
+});
+
+test('when an Entry is clicked and then clicked again, no EntryControls are \
+  displayed', () => {
+  const firstEntry = entriesWrapper.find('.entry').at(0);
+  firstEntry.simulate('click');
+  const entryControls = entriesWrapper.find('.controls');
+  firstEntry.simulate('click');
+  expect(entryControls).not;
+});
+
+test('when two different Entry components are clicked, only the most recently \
+  clicked Entry has its EntryControls displayed', () => {
+  const firstEntry = entriesWrapper.find('.entry').at(0);
+  const secondEntry = entriesWrapper.find('.entry').at(1);
+  firstEntry.simulate('click');
+  secondEntry.simulate('click');
+  expect(secondEntry.find('.controls'));
+  expect(firstEntry.find('.controls')).not;
 });
--- editor/javascript/tests/components/Entry.test.js
+++ editor/javascript/tests/components/Entry.test.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
 import Entry from '../../components/Entry.jsx';
 
-test('the Entry component displays one entry', () => {
-  const props = {
-      id: "1",
-      title: "I walked for a while",
-      content: "I dozed off."
-    }
+const props = {
+  id: '1',
+  title: 'I walked for a while',
+  content: 'I dozed off.',
+};
 
+const entryWrapper = shallow(
+  <Entry key={props.id} title={props.title} content={props.content} />,
+);
+
+test('the Entry component displays one entry', () => {
   const component = renderer
-    .create( <Entry key={props.id} title={props.title} content={props.content} />)
+    .create(
+      <Entry key={props.id} title={props.title} content={props.content} />,
+    )
     .toJSON();
 
   expect(component).toMatchSnapshot();
+});
+
+test('when first rendered, an Entry does not display EntryControls', () => {
+  expect(entryWrapper.find('.controls')).not;
+});
+
+test('when clicked, an Entry displays its EntryControls', () => {
+  entryWrapper.simulate('click');
+  expect(entryWrapper.find('.controls'));
+});
+
+test('when clicked and then clicked again, the Entry does not display its \
+  EntryControls', () => {
+  entryWrapper.simulate('click');
+  entryWrapper.simulate('click');
+  expect(entryWrapper.find('.controls')).not;
 });
