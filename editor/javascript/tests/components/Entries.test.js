c5c20592a9b5f3380466e25ab5c41532e2cfbfc4
Benjamin Willems <bw@benjaminwil.info>
2019-08-27 09:26:40 -0700

    Use Entries test stubs for all Entries tests

--- editor/javascript/tests/components/Entries.test.js
+++ editor/javascript/tests/components/Entries.test.js
@@ -2,23 +2,21 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Entries from '../../components/Entries.jsx';
 
-test('the Entries component displays many entries', () => {
-  const entries = [
-    {
-      id: "1",
-      title: "I walked for a while",
-      content: "I dozed off."
-    },
-    {
-      id: "2",
-      title: "In my sleep",
-      content: "I dreamed of you."
-    }
-  ]
 
-  const component = renderer
-    .create( <Entries entries={entries} />)
-    .toJSON();
+const entries = [
+  {
+    id: '1',
+    title: 'I walked for a while',
+    content: 'I dozed off.',
+  },
+  {
+    id: '2',
+    title: 'In my sleep',
+    content: 'I dreamed of you.',
+  },
+];
 
+test("the Entries component displays many entries", () => {
+  const component = renderer.create(<Entries entries={entries} />).toJSON();
   expect(component).toMatchSnapshot();
 });
