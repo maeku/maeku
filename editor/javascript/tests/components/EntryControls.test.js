40b31d2e970b2eaab5513b78836256c42cabba60
Benjamin Willems <bw@benjaminwil.info>
2019-09-04 12:54:11 -0700

    Add EntryControls component tests

---
+++ editor/javascript/tests/components/EntryControls.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+import EntryControls from '../../components/EntryControls.jsx';
+
+// Note: the contents of the URLs props are tested in the Rails EntryPresenter
+// model that generates them.
+function entryControlsWrapper() {
+  const controls = (
+    <EntryControls urls={{edit: '/entries/1/edit', view: '/entries/1'}} />
+  );
+  return mount(controls);
+}
+
+test('the EntryControls component a "view entry" button', () => {
+  expect(entryControlsWrapper().find('.view'));
+});
+
+test('the EntryControls component has an "edit entry" button', () => {
+  expect(entryControlsWrapper().find('.edit'));
+});
+
+test('the EntryControls component has a "delete entry" button', () => {
+  expect(entryControlsWrapper().find('.delete'));
+});
