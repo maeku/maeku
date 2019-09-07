e0a13546c1ebb2976a56bcc8d681c5c5f6756bb9
Benjamin Willems <bw@benjaminwil.info>
2019-09-07 14:13:03 -0700

    Create views for editing/showing an entry

--- editor/app/controllers/maeku/editor/entries_controller.rb
+++ editor/app/controllers/maeku/editor/entries_controller.rb
@@ -1,8 +1,11 @@
 module Maeku
   class Editor::EntriesController < Editor::BaseController
+    def show
+      @entry = Editor::EntryPresenter.new(Entry.find(params[:id]))
+    end
+
     def destroy
       Entry.destroy(params[:id])
     end
   end
 end
-
