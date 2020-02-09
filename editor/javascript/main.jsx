7f5f2fca2d233d6ebeff28dc6d588cb4092fc1c2
Benjamin Willems <bw@benjaminwil.info>
2020-02-08 18:53:12 -0800

    Add react-codemirror2 package for entry editor
    (And `yarn upgrade`)

--- editor/javascript/main.jsx
+++ editor/javascript/main.jsx
@@ -1,6 +1,7 @@
 const componentsDir = require.context('./components', true);
 const ReactRailsUJS = require('react_ujs');
 
+import CodeMirror from 'react-codemirror2';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
