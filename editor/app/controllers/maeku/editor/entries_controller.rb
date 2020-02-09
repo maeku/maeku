e3bc0a366790f918d47bb221495964b50d71b65e
Benjamin Willems <bw@benjaminwil.info>
2020-02-08 20:43:46 -0800

    Add initial entries/edit controller

--- editor/app/controllers/maeku/editor/entries_controller.rb
+++ editor/app/controllers/maeku/editor/entries_controller.rb
@@ -1,11 +1,22 @@
 module Maeku
   class Editor::EntriesController < Editor::BaseController
+
+    def edit
+      @entry = Editor::EntryPresenter.new(current_entry).data
+    end
+
     def show
-      @entry = Editor::EntryPresenter.new(Entry.find(params[:id]))
+      @entry = Editor::EntryPresenter.new(current_entry)
     end
 
     def destroy
       Entry.destroy(params[:id])
+    end
+
+    private
+
+    def current_entry
+      Entry.find(params[:id])
     end
   end
 end
