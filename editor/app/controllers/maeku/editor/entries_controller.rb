38eed68262ad36a3edf7891e5635d9eef1ed3e90
Benjamin Willems <bw@benjaminwil.info>
2020-03-18 09:59:27 -0700

    Set up scaffolding for codemirror-based editor

--- editor/app/controllers/maeku/editor/entries_controller.rb
+++ editor/app/controllers/maeku/editor/entries_controller.rb
@@ -1,6 +1,10 @@
 module Maeku
   class Editor::EntriesController < Editor::BaseController
 
+    def destroy
+      Entry.destroy(params[:id])
+    end
+
     def edit
       @entry = Editor::EntryPresenter.new(current_entry).data
     end
@@ -9,14 +13,25 @@ module Maeku
       @entry = Editor::EntryPresenter.new(current_entry)
     end
 
-    def destroy
-      Entry.destroy(params[:id])
+    def update
+      respond_to do |format|
+        if current_entry.update(entry_params)
+          format.json { render json: current_entry, status: response.code }
+          format.html { redirect_to entry_url(current_entry), notice: 'Updated!' }
+        else
+          format.html { render :edit }
+        end
+      end
     end
 
     private
 
     def current_entry
       Entry.find(params[:id])
+    end
+
+    def entry_params
+      params.require(:entry).permit(entry_content_attributes: [:content])
     end
   end
 end
--- editor/javascript/components/EntryEditor.jsx
+++ editor/javascript/components/EntryEditor.jsx
@@ -4,28 +4,50 @@ import {UnControlled as CodeMirror} from 'react-codemirror2'
 require('codemirror/mode/markdown/markdown.js')
 require('codemirror/addon/display/panel.js')
 
-
-
 class EntryEditor extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { value: props.entry.content };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateEntry = this.updateEntry.bind(this);
+  }
+
+  updateEntry() {
+    const url = this.props.entry.urls.show;
+
+    return fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: {
+        'entry': {
+          'entry_contents': { 'content': this.state.value }
+        }
+      }
+    });
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //this.setState({value: event.target.value});
+    this.updateEntry;
   }
 
   render() {
     return (
-      <CodeMirror
-        value={this.props.entry.content}
-        options={{
-          mode: 'markdown',
-          lineWrapping: true,
-          theme: 'maeku-editor'
-        }}
-        onChange={(editor, data, value) => {}}
-      />
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' value={this.state.value} onChange={this.handleChange}/>
+        <input type='submit' value='Submit' />
+      </form>
     )
-
-
   }
 }
 
-export default EntryEditor
+export default EntryEditor;
